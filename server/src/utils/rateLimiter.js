01395548efcda3d8b7689dd806c0edd8e42b7fe9
Carl Mari Datangel <datangel.carl0504@gmail.com>
2025-04-30 15:54:28 +0800

    chore: remove unnecessary comment

--- server/src/utils/rateLimiter.js
+++ server/src/utils/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimiter = require("express-rate-limit");
 
 const loginLimiter = rateLimiter({
-    windowMs: 60 * 1000, // 1 minute
-    max: 5, // limit each IP to 5 requests per windowMs
+    windowMs: 60 * 1000,
+    max: 5, 
     message: {
         status: 429,
         message: "Too many login attempts, please try again after 1 minute"
