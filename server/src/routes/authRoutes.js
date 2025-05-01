4e258b60e3587368ed78447d564d8802d4a90535
Carl Mari Datangel <datangel.carl0504@gmail.com>
2025-05-01 22:33:34 +0800

    chore: adjust rate limiter in routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const {validate} = require('../utils/validate');
 const { registerSchema, mobileLoginSchema, webLoginSchema } = require('../validator/authValidator');
-const {loginLimiter} = require('../utils/rateLimiter');
+const loginLimiter = require('../utils/rateLimiter');
 
 router.post('/register', validate(registerSchema), authController.register);
 
-router.post('/web-login', validate(webLoginSchema),loginLimiter,authController.webLogin);
-router.post('/mobile-login', validate(mobileLoginSchema), loginLimiter,authController.mobileLogin);
+router.post('/web-login', loginLimiter,validate(webLoginSchema),authController.webLogin);
+router.post('/mobile-login', loginLimiter,validate(mobileLoginSchema),authController.mobileLogin);
 
 router.get('/get-college', authController.getColleges); // <-- added
 router.get('/get-department/:collegeId', authController.getDepartments); // <-- added
