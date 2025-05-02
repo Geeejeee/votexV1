ea72a27d75d07596702250c19b7f61a780f972a0
Carl Mari Datangel <datangel.carl0504@gmail.com>
2025-05-02 10:12:50 +0800

    refactor/ updated code

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
-const http = require("http");
-const { Server } = require("socket.io");
 const dbConnect = require("./libs/db");
 const studentRoutes = require("./routes/studentRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -14,9 +12,7 @@ const errorMiddleware = require("./utils/error.middleware");
 dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-global.io = io;
+
 
 // Middleware
 app.use(cors());
@@ -32,19 +28,10 @@ app.use("/api/position", positionRoutes);
 // Error middleware
 app.use(errorMiddleware);
 
-// Handle WebSocket connections
-io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("Socket disconnected:", socket.id);
-  });
-});
-
 // Connect to DB and start server
 dbConnect((client) => {
   if (client) {
-    server.listen(process.env.PORT || 5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`server is running on port ${process.env.PORT || 5000}`);
     });
   } else {
