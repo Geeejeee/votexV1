b1637f88c24689a2128696be3032c93f7aab271b
Carl Mari Datangel <datangel.carl0504@gmail.com>
2025-05-19 10:01:40 +0800

    chore/ url edit

--- votex-mobile/src/screens/login.jsx
+++ votex-mobile/src/screens/login.jsx
@@ -18,7 +18,7 @@ const LoginScreen = ({ navigation }) => {
 
     const handleLogin = async (values) => {
         try {
-            const response = await fetch(`http://192.168.1.26:5000/api/auth/mobile-login`, {
+            const response = await fetch(`${API_BASE_URL}/api/auth/mobile-login`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
